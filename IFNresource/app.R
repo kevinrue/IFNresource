01b5d6362c474ec11dceff27c4f6e4bd68c5b1f8
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-06-16 12:37:40 +0100

    replace template shiny app with iSEEindex code

---
+++ IFNresource/app.R
@@ -0,0 +1,26 @@
+library(iSEEindex)
+library(DESeq2)
+
+##
+# BiocFileCache ----
+##
+
+library(BiocFileCache)
+bfc <- BiocFileCache(cache = tempdir())
+
+##
+# iSEEindex ----
+##
+
+dataset_fun <- function() {
+  x <- yaml::read_yaml("config.yaml")
+  x$datasets
+}
+
+initial_fun <- function() {
+  NULL
+}
+
+app <- iSEEindex(bfc, dataset_fun, initial_fun)
+
+shiny::runApp(app, launch.browser = TRUE)
